9bc3ae5acf0f19be80b1374441c5533347d4bb78
Almaz0430 <almaz.zh7@gmail.com>
2026-02-03 00:50:16 +0500

    deploy

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = '/api';
 
 interface Booking {
   id: number;
